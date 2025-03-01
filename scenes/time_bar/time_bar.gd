9faa930040cff84d5a1b661fbf67984c2acc4f59
geman <geman@192.168.1.10>
2025-03-01 23:45:31 +0800

    feat: time bar spliter

--- scenes/time_bar/time_bar.gd
+++ scenes/time_bar/time_bar.gd
@@ -5,6 +5,11 @@ const WIDTH_PER_UNIT: int = 30
 
 @onready var need_progress_bar: TextureProgressBar = $NeedProgressBar
 @onready var actual_progress_bar: TextureProgressBar = $ActualProgressBar
+@onready var spliters: Control = $Spliters
+
+const TIME_BAR_SPLITER = preload("res://scenes/time_bar/time_bar_spliter.tscn")
+
+var pre_need = 0
 
 var test_current = 0
 
@@ -15,7 +20,18 @@ func _process(delta: float) -> void:
 
 func set_time_bar(current: float, need: float):
 	current = clamp(current, 0, need)
-	need_progress_bar.size = Vector2(ceil(need) * WIDTH_PER_UNIT, 16)
-	actual_progress_bar.size = Vector2(ceil(need) * WIDTH_PER_UNIT, 16)
-	need_progress_bar.value = (need / ceil(need)) * 100
+	var ceil_need = ceil(need)
+	if need != pre_need:
+		for n in spliters.get_children():
+			n.queue_free()
+		need_progress_bar.size = Vector2(ceil_need * WIDTH_PER_UNIT, 16)
+		need_progress_bar.value = (need / ceil_need) * 100
+		for i in range(max(0, ceil_need - 1)):
+			var spliter = TIME_BAR_SPLITER.instantiate()
+			spliters.add_child(spliter)
+			spliter.position = Vector2((i + 1) * WIDTH_PER_UNIT - 1, 3)
+			
+	
+	actual_progress_bar.size = Vector2(ceil_need * WIDTH_PER_UNIT, 16)
+	
 	actual_progress_bar.value = (current / ceil(need)) * 100
